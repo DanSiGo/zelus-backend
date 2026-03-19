0cae212829e284bb634856502b413b153da97117
dansigo <daniel.sigo@gmail.com>
2026-03-18 22:16:45 -0300

    fix: resolve database connection with supabase pooler and sync tables

--- src/main.py
+++ src/main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
 from src.routes import items
 from src.database import engine, Base
-import src.models
-
-Base.metadata.create_all(bind=engine)
+from src.models import user, report, comment # Importa para o SQLAlchemy "enxergar" as tabelas
 
 app = FastAPI(title="Zelus API")
+
+
+print("--- Verificando Banco de Dados ---", flush=True)
+try:
+    # Força a criação das tabelas
+    Base.metadata.create_all(bind=engine)
+    print("Tabelas verificadas/criadas com sucesso!", flush=True)
+except Exception as e:
+    print(f"Erro ao conectar no banco: {e}", flush=True)
+
+
 
 app.include_router(items.router)
 
